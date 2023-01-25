f1ed82716868b94abe43e54bb2c5bbe04c543ddb
arjungupta06 <arjun.gupta@brillio.com>
2023-01-25 23:25:02 +0000

    Update streamlit_app.py
    added emojis and byofs

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit
 
 streamlit.title ('My Parents New Healthy Diner!')
 streamlit.header ('Breakfast Menu')
-streamlit.text ('French Omlette')
-streamlit.text ('Eggs Hollandaise')
-streamlit.text ('Pancakes')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🍞 French Omlette & Toast')
+streamlit.text ('🐔Eggs Hollandaise')
+streamlit.text ('🥗Pancakes')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
