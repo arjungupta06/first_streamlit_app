177ee2bfd195a943a69b32fd9652f68aca7db9b0
arjungupta06 <arjun.gupta@brillio.com>
2023-01-26 10:32:12 +0000

    Update streamlit_app.py
    added more functions and a button for snnowflake data retrieval

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,14 +43,20 @@ try:
 except URLError as e:
     streamlit.error()
 
-#snowflake test
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
+    
+def get_fruit_load_list():
+  with my_cnx as my_cur:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
 
+if streamlit.button('Get Fruit load list'):
+  #snowflake test
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
 #user inputs for fruit addition
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
