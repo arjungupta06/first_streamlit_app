d5d5a965f8d51c4fce47cdf8bc1ee1015f0d3107
arjungupta06 <arjun.gupta@brillio.com>
2023-01-26 12:07:14 +0000

    Update streamlit_app.py
    closed connection, parameterized query for insertion

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,18 +54,20 @@ if streamlit.button('Get Fruit load list'):
   #snowflake test
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list(my_cnx)
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
-    return "Thanks for adding" + new_fruit
+    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('"+new_fruit+"')")
+    return "Thanks for adding " + new_fruit
 
 #user inputs for fruit addition
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   streamlit.text(insert_row_snowflake(add_my_fruit))
+  my_cnx.close()
 
 #streamlit.stop()
 
